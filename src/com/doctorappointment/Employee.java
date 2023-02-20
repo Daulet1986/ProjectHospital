c746193209a11328bb782e07c947e0cafc109708
chilling2020 <67411138+chilling2020@users.noreply.github.com>
2023-02-20 08:33:21 +0600

    Classes changed. Person and Employee class using interface to create tables in database

--- src/com/doctorappointment/Employee.java
+++ src/com/doctorappointment/Employee.java
@@ -1,37 +1,76 @@
 package com.doctorappointment;
-public class Employee extends Person{
-    private String position;
-    private double salary;
-    private int area;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Employee extends Person implements createTable, readTab {
+    private String position;
+    private int price;
+    private String time;
     //Constructors
     public Employee(){
         super();
     }
-
-    public Employee(String name, String surname, String position, double salary, int area){
-        setName(name);
-        setSurname(surname);
-        setPosition(position);
-        setSalary(salary);
-        setArea(area);
-    }
-
     //Getters
     public String getPosition(){
         return position;
     }
-    public double getSalary(){
-        return salary;
+
+    public String getTime() {
+        return time;
     }
-    public int getArea(){return area;}
+
+    public int getPrice(){return price;}
 
     //Setters
     public void setPosition(String position){
         this.position = position;
     }
-    public void setSalary(double salary){
-        this.salary = salary;
+    public void setPrice(int price){this.price = price;}
+    @Override
+    public void createTable (Connection conn, String table) {
+        Statement statement;
+        try {
+            String query = "create table " + table+ "(id serial primary key, name varchar(20), " +
+                    "surname varchar(20),position varchar(20), time varchar(50),price varchar(20));";
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
-    public void setArea(int area){this.area = area;}
+    public void addDoc(Connection conn,String table_name,String name,String surname,String position,String time,String price){
+        Statement statement;
+        try{
+            String query=String.format("insert into %s(name,surname,position,time,price) values ('%s','%s','%s','%s','%s');",table_name,name,surname,position,time,price);
+            statement=conn.createStatement();
+            statement.executeUpdate(query);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void readTable(Connection conn, String table_name, String position)  {
+        Statement statement;
+        ResultSet resultSet = null;
+        try {
+            String query = String.format("select * from %s where position = '%s'", table_name,position );
+            statement = conn.createStatement();
+            resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                time= resultSet.getString("time");
+                price= resultSet.getInt("price");
+                name= resultSet.getString("name");
+                this.position=resultSet.getString("position");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+
 }
